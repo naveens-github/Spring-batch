e65ae9637c4679e3002153f9895b6d3bf86356e6
naveen <naveenwork8@gmail.com>
2023-05-24 00:06:10 -0400

    first commit

---
+++ batch-processing-demo/src/test/java/com/myproject/spring/batch/BatchProcessingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
